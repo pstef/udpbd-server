1881b63e39fb14976ea5988960b6f361675d0a4f
Keith Gable <keith@ziggythehamster.sh>
2023-11-06 22:52:22 -0800

    Use ::bind instead of bind so that it compiles with GCC 13

--- main.cpp
+++ main.cpp
@@ -121,7 +121,7 @@ public:
         si_me.sin_family = AF_INET;
         si_me.sin_port = htons(UDPBD_PORT);
         si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-        if (bind(s, (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
+        if (::bind(s, (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
             throw runtime_error("bind");
         }
 
